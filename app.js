c5a73c23fe9b384967b529733185ceca6de6ff1b
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-04-08 13:48:31 -0400

    Add clock to app

--- app.js
+++ app.js
@@ -135,6 +135,31 @@ function toFahrenheight(temp) {
     return result.toFixed();
 }
 
+function clock() {
+    function updateClock() {
+        const now = new Date();
+        const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
+
+        const isDST = (now.getMonth() > 2 && now.getMonth() < 11);
+        const estOffset = isDST ? -4 : -5; 
+        const estTime = new Date(utc + (3600000 * estOffset));
+
+        let hours = estTime.getHours();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        const minutes = estTime.getMinutes();
+        const seconds = estTime.getSeconds();
+        const formattedTime = 
+            (hours < 10 ? "0" + hours : hours) + ":" + 
+            (minutes < 10 ? "0" + minutes : minutes) + ":" + 
+            (seconds < 10 ? "0" + seconds : seconds) + " " + ampm;
+
+            document.getElementById("clock").innerText = formattedTime;
+    }
+    setInterval(updateClock, 1000);
+}
+
 // grab div main by id and populate inside with next 4 days of weather with info from this
 
 function populateData() {
@@ -155,7 +180,6 @@ function populateData() {
             
         </weather-card>
     `
-
 
     // Upcoming data
     for (let i = 1; i < days.length; i++) {
@@ -179,18 +203,12 @@ function populateData() {
             </div>
 
         `
-
         document.getElementById("upcoming").appendChild(weather_div);
-        
-    }
-    // document.getElementById('main').innerHTML = ``
-    // document.getElementById('weather').innerHTML = `
-    //     <p>Temperature: ${data.dataseries[0].temp2m}°C</p> 
-    // `;
-        
+    }   
 }
 
 async function render() {
+    clock();
     await meteoWeather();
     await displayWeather();
     populateData();
